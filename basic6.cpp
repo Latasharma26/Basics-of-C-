f9bce2a422de7e7ca3984ff3b14aa238f0ce7546
Lata Sharma <96385877+Latasharma26@users.noreply.github.com>
2024-02-22 06:11:46 +0530

    Update basic6.cpp

--- basic6.cpp
+++ basic6.cpp
@@ -13,3 +13,16 @@ cout<<area;
 
 return 0;
 }
+
+// Take the person's name and age as input and print out the name and age as specified in the output format.
+#include <iostream>
+using namespace std;
+
+int main() {
+string name;
+int age;
+cin>>name;
+cin>>age;
+cout<<"The name of the person is "<<name<<" and the age is "<<age<<".";    
+return 0;
+}
